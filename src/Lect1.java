79cab362a7efaf27a43227d45fb51001c39bea73
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-12-15 21:55:34 +0530

    Commit

--- src/Lect1.java
+++ src/Lect1.java
@@ -1,2 +1,21 @@
 public class Lect1 {
+    public static void main(String[] args) {
+        Student Likhith = new Student("Likhith",21,39.23f);
+        System.out.println(Likhith);
+
+    }
+
+
+}
+class Student{
+    String Name;
+    int roll_no;
+    float marks;
+
+    Student(String Name,int roll_no,float marks){
+        this.Name = Name;
+        this.roll_no = roll_no;
+        this.marks = marks;
+    }
+
 }
